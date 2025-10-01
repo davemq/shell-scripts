760779f276a9095c1b978e9c88fe0c85a13cf6a4
Dave Marquardt <davemarq@linux.ibm.com>
2025-10-01 17:57:34 -0500

    Updated to RAGBRAI route program

--- ragbrai.py
+++ ragbrai.py
@@ -1,20 +1,40 @@
+#!/usr/bin/env python
+
+from collections import defaultdict
 import csv
 import random
+import sys
 
 days = ('Starting City', 'Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday')
-towns = dict()
-for day in days:
-    towns[day] = []
+starts = []
+adjacencies = defaultdict(list)
 
 with open('/home/davemarq/Downloads/ragbrai-by-year.csv', newline='') as csvfile:
     reader= csv.DictReader(csvfile, delimiter=',')
     for row in reader:
-        for day in days:
-            towns[day].append(row[day])
+        starts.append(row['Starting City'])
 
-print(towns)
-print(total)
-print('Random choice: ')
-for day in days:
-    print(random.choice(towns[day]))
+with open('/home/davemarq/Downloads/ragbrai-by-year.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    header = False
+    for row in reader:
+        if not header:
+            header = row
+        else:
+            for i in range(5,11):
+                adjacencies[row[i]].append(row[i+1])
 
+route = []
+cur = random.choice(starts)
+route.append(cur)
+for i in range(7):
+    print(adjacencies[cur])
+    try:
+        next = random.choice(adjacencies[cur])
+    except IndexError:
+        print("failed!")
+        sys.exit(1)
+    route.append(next)
+    cur = next
+
+print(route)
