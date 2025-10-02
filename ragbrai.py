cf1f48cf8be72b0587e983dee43cff46ab91816a
Dave Marquardt <davemarq@linux.ibm.com>
2025-10-02 13:54:01 -0500

    Encapsulated code into routines. Added simulation.
    - Moved parsing into routine
    - Moved random route generation into routine
    - Added code to generate routes many times, ala Monte Carlo method
    - Print common routes

--- ragbrai.py
+++ ragbrai.py
@@ -9,32 +9,56 @@ days = ('Starting City', 'Sunday', 'Monday','Tuesday','Wednesday','Thursday','Fr
 starts = []
 adjacencies = defaultdict(list)
 
-with open('/home/davemarq/Downloads/ragbrai-by-year.csv', newline='') as csvfile:
-    reader= csv.DictReader(csvfile, delimiter=',')
-    for row in reader:
-        starts.append(row['Starting City'])
+def parse():
+    global adjacencies, starts
+    
+    with open('/home/davemarq/Downloads/ragbrai-by-year.csv', newline='') as csvfile:
+        reader= csv.DictReader(csvfile, delimiter=',')
+        for row in reader:
+            starts.append(row['Starting City'])
 
-with open('/home/davemarq/Downloads/ragbrai-by-year.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    header = False
-    for row in reader:
-        if not header:
-            header = row
-        else:
-            for i in range(5,11):
-                adjacencies[row[i]].append(row[i+1])
+    with open('/home/davemarq/Downloads/ragbrai-by-year.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = False
+        for row in reader:
+            if not header:
+                header = row
+            else:
+                for i in range(5,11):
+                    adjacencies[row[i]].append(row[i+1])
+    
 
-route = []
-cur = random.choice(starts)
-route.append(cur)
-for i in range(7):
-    print(adjacencies[cur])
-    try:
-        next = random.choice(adjacencies[cur])
-    except IndexError:
-        print("failed!")
-        sys.exit(1)
-    route.append(next)
-    cur = next
+def makeroute():
+    global adjacencies, starts
 
-print(route)
+    route = []
+    cur = random.choice(starts)
+    route.append(cur)
+    for i in range(7):
+        try:
+            next = random.choice(adjacencies[cur])
+        except IndexError:
+            return None
+        route.append(next)
+        cur = next
+
+    return route
+
+
+parse()
+
+routes = defaultdict(int)
+
+for i in range(1000000):
+    r = None
+    while r is None:
+        r = makeroute()
+    routes[tuple(r)] += 1
+print(f"Generated {len(routes)} routes")
+
+sorted = dict(sorted(routes.items(), key=lambda item: item[1], reverse=True))
+for r in sorted:
+    if sorted[r] < 1000:
+        break
+    print(r, sorted[r])
+
